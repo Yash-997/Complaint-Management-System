d884f3c88658ba409d007d1185e7c35b55dfd739
Yash-997 <yashdabhade.official@gmail.com>
2026-05-28 22:44:59 +0530

    Fixed deployed frontend CORS

--- src/main/java/com/yash/Complaint_Management_System/config/SecurityConfig.java
+++ src/main/java/com/yash/Complaint_Management_System/config/SecurityConfig.java
@@ -5,25 +5,18 @@ import com.yash.Complaint_Management_System.security.JwtFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.authentication.AuthenticationManager;
-
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -131,11 +124,21 @@ public class SecurityConfig {
         CorsConfiguration configuration =
                 new CorsConfiguration();
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of(
+                        "http://localhost:5173",
+                        "https://complaint-management-system-fronten-self.vercel.app"
+                )
         );
         configuration.setAllowedMethods(
-                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                List.of(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "OPTIONS"
+                )
         );
+
         configuration.setAllowedHeaders(
                 List.of("*")
         );
@@ -147,6 +150,7 @@ public class SecurityConfig {
                 "/**",
                 configuration
         );
+
         return source;
     }
 }
