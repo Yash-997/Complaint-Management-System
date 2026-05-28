ab22bb5cfdb60d8e9e986686c782bb2a56012847
Yash-997 <yashdabhade.official@gmail.com>
2026-05-28 22:35:51 +0530

    Fixed CORS issue

---
+++ src/main/java/com/yash/Complaint_Management_System/config/CorsConfig.java
@@ -0,0 +1,39 @@
+package com.yash.Complaint_Management_System.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config =
+                new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+
+        config.addAllowedOrigin(
+                "https://complaint-management-system-fronten-self.vercel.app"
+        );
+
+        config.addAllowedHeader("*");
+
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration(
+                "/**",
+                config
+        );
+
+        return new CorsFilter(source);
+    }
+}
